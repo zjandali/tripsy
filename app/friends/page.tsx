7444a2b9f05b660c103fa58b84b8629ded5649eb
zjandali <yzjandali@berkeley.edu>
2025-01-03 19:16:58 -0800

    removed unused imports pt 2

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image'
 
 interface User {
   id: string;
